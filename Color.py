9c3251fd6d2d324a80623d499ab1d9a15d537f8d
Alferdize <simonandrew132@gmail.com>
2020-11-07 16:27:10 +0530

    Color

---
+++ Color.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+
+def empty(a):
+    pass
+
+
+path = "resources/lambo.png"
+
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars", 640, 240)
+cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, empty)
+cv2.createTrackbar("Hue Max", "TrackBars", 19, 179, empty)
+cv2.createTrackbar("Saturation Min", "TrackBars", 110, 255, empty)
+cv2.createTrackbar("Saturation Max", "TrackBars", 240, 255, empty)
+cv2.createTrackbar("Val Min", "TrackBars", 153, 255, empty)
+cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
+while True:
+    img = cv2.imread(path)
+
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Saturation Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Saturation Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    imageResult = cv2.bitwise_and(img, img, mask=mask)
+    cv2.imshow("Original ", imgHSV)
+    cv2.imshow("Original HSV", mask)
+    cv2.imshow("Result", imageResult)
+
+    if cv2.waitKey(1000) & 0xFF == ord('q'):
+        break
---
+++ Wrap.py
@@ -0,0 +1,21 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("resources/cards.jpg")
+
+width, height = 250, 350
+
+pts = np.float32([[111, 219], [287, 188], [154, 482], [352, 440]])
+
+pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+
+matrix = cv2.getPerspectiveTransform(pts,pts2)
+
+imgOutput = cv2.warpPerspective(img, matrix, (width, height))
+
+
+
+cv2.imshow("Image", img)
+cv2.imshow("Image Output", imgOutput)
+
+cv2.waitKey(1000)
